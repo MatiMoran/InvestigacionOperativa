04663e6afdbb3129950c669db933e83241950930
Matias Moran <matiec52000@hotmail.com>
2025-08-28 01:23:30 -0300

    add clases practicas

---
+++ InvestigacionOperativa/clasesPracticas/clase2.sol
@@ -0,0 +1,3 @@
+objective value:                                  435
+w_0                                                 1 	(obj:-165)
+x_0                                                20 	(obj:30)
