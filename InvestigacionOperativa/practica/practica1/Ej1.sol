9a95a6184df3f42a55368e75cf26bdc60841a243
Matias Moran <matiec52000@hotmail.com>
2025-08-31 20:18:51 -0300

    fix first problem

---
+++ InvestigacionOperativa/practica/practica1/Ej1.sol
@@ -0,0 +1,3 @@
+objective value:                                 1.18
+x_1                                                 7 	(obj:0.1)
+x_2                                                 4 	(obj:0.12)
