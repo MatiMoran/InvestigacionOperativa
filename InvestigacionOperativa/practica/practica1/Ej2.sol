92870fc38f8649de710d5d51288561d3b5dd89f5
Matias Moran <matiec52000@hotmail.com>
2025-08-31 22:04:10 -0300

    add sol 2 and 3

---
+++ InvestigacionOperativa/practica/practica1/Ej2.sol
@@ -0,0 +1,5 @@
+objective value:                                 4527
+x_0                                               220 	(obj:4.4)
+x_1                                               130 	(obj:3.8)
+x_2                                               250 	(obj:4.1)
+x_4                                               400 	(obj:5.1)
