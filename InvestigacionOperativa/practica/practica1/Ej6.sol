bccf5c933cb6d66e9b9b237f920741c0089706db
Matias Moran <matiec52000@hotmail.com>
2025-09-01 02:16:44 -0300

    ex 6

---
+++ InvestigacionOperativa/practica/practica1/Ej6.sol
@@ -0,0 +1,7 @@
+objective value:                                   50
+Z_0                                                 1 	(obj:0)
+A_03                                                1 	(obj:38)
+Y_1                                                 1 	(obj:0)
+Y_2                                                 1 	(obj:0)
+Z_3                                                 1 	(obj:0)
+A_34                                                1 	(obj:12)
