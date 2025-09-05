4b8359adf7bcdb134d1f3da9cfec722d102ff44f
Matias Moran <matiec52000@hotmail.com>
2025-09-05 01:40:13 -0300

    fill clase 4

---
+++ InvestigacionOperativa/clasesPracticas/clase4-my.sol
@@ -0,0 +1,6 @@
+objective value:                                  939
+X_0                                                 5 	(obj:0)
+X_1                                                15 	(obj:0)
+X_2                                                 6 	(obj:0)
+X_3                                                 4 	(obj:0)
+W                                                 939 	(obj:1)
